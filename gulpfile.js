b8e9ad3d03f77a58839788a3def51b69e5d57df5
monta <djmonta@gmail.com>
2016-05-18 19:58:39 +0900

    :new: add: いろいろ整える

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var plumber = require('gulp-plumber');
+var notifier = require('node-notifier');
+
+gulp.task('sass', function () {
+	return gulp.src('sass/*.scss')
+	.pipe(plumber({
+		errorHandler: function(error) {
+			// ここでエラーをキャッチできる
+			var title = '[task]' + taskName + ' ' + error.plugin;
+			var errorMsg = 'error: ' + error.message;
+			console.error(title + '\n' + errorMsg);
+			// node-notifierがデスクトップ通知をしてくれる
+			notifier.notify({
+				title: title,
+				message: errorMsg,
+				time: 3000
+			});
+		}
+	}))
+	.pipe(sourcemaps.init())
+	.pipe(sass({outputStyle: 'expanded'}))
+	.pipe(sourcemaps.write())
+	.pipe(plumber.stop())
+	.pipe(gulp.dest('css'));
+});
+
+gulp.task('sass:watch', function () {
+	gulp.watch('sass/*.scss', ['sass']);
+});
+
+// デフォルトで起動するタスク
+gulp.task('default', ['sass', 'sass:watch']);
